4faac462ad863ea152198e375f1ba15c6530bcc8
alp <alptuzer@gmail.com>
2024-04-26 19:57:06 +0300

    1

---
+++ reactdice/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import RollDice from './components/RollDice';
+function App() {
+ 
+    return (
+      <div className="App">
+       Deneme
+       <RollDice></RollDice>
+      </div>
+    );
+ 
+}
+
+export default App;
